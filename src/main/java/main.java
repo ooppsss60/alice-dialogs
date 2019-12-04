88bfa6f408baefbe8e19c9894f35e2f043d46a5d
Dmitry <40275556+ooppsss60@users.noreply.github.com>
2019-12-04 23:25:55 +0300

    First working commit

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,30 @@
+import org.json.simple.*;
+import org.json.simple.parser.*;
+import spark.Spark;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 import static spark.Spark.*;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+        secure("deploy/keystore.jks", "12345678", null, null);
+        Spark.ipAddress("0.0.0.0");
+        post("/api", (req, res) -> {
 
+            JSONParser parser = new JSONParser();
+            JSONObject response = new JSONObject();
+            JSONObject request = new JSONObject();
+            try {
+                response = (JSONObject) parser.parse(new FileReader("request.json"));
+                System.out.println((String) response.get("version"));
+                System.out.println(req);
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            return response;
+        });
+    }
 }
