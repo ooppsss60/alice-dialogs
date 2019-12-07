f60baead23917a69ea73f5e0a0019235f635163d
Dmitry <40275556+ooppsss60@users.noreply.github.com>
2019-12-07 18:13:59 +0300

    Release

--- alice/src/main/java/Main.java
+++ alice/src/main/java/Main.java
@@ -4,7 +4,7 @@ import org.json.*;
 import static spark.Spark.*;
 
 public class Main {
-    public static JSONObject ledState = new JSONObject("{\"red\":1,\"green\":0,\"blue\":0}");
+    public static JSONObject ledState = new JSONObject("{\"red\":0,\"green\":0,\"blue\":0}");
 
     public static void main(String[] args) throws IOException {
         secure("deploy/keystore.jks", "12345678", null, null);
@@ -25,14 +25,66 @@ public class Main {
         JSONObject response = new JSONObject();
         String version = request.getString("version");
         JSONObject session = (JSONObject) request.get("session");
+        JSONObject requestBody = (JSONObject) request.get("request");
         response.put("version",version);
         response.put("session",session);
         JSONObject responseBody = new JSONObject();
-        responseBody.put("text","test");
+
+        if(session.getBoolean("new")){
+            responseBody.put("text","Привет");
+        }else{
+            switch (requestBody.getString("command").toLowerCase()) {
+                case ("включи красный"):
+                    responseBody.put("text","Включаю");
+                    ledState.put("red",1);
+                    break;
+                case ("включи зелёный"):
+                    responseBody.put("text","Включаю");
+                    ledState.put("green",1);
+                    break;
+                case ("включи синий"):
+                    responseBody.put("text","Включаю");
+                    ledState.put("blue",1);
+                    break;
+                case ("включи все"):
+                    responseBody.put("text","Включаю все");
+                    ledState.put("red",1);
+                    ledState.put("green",1);
+                    ledState.put("blue",1);
+                    break;
+                case ("выключи красный"):
+                    responseBody.put("text","Выключаю");
+                    ledState.put("red",0);
+                    break;
+                case ("выключи зелёный"):
+                    responseBody.put("text","Выключаю");
+                    ledState.put("green",0);
+                    break;
+                case ("выключи синий"):
+                    responseBody.put("text","Выключаю");
+                    ledState.put("blue",0);
+                    break;
+                case ("выключи все"):
+                    responseBody.put("text","Выключаю все");
+                    ledState.put("red",0);
+                    ledState.put("green",0);
+                    ledState.put("blue",0);
+                    break;
+                case ("помощь"):
+                    responseBody.put("text","Вот что я могу");
+                    JSONArray buttons = new JSONArray();
+                    buttons.put(new JSONObject("{\"title\": \"Включи красный\",\"hide\": true}"));
+                    buttons.put(new JSONObject("{\"title\": \"Выключи синий\",\"hide\": true}"));
+                    buttons.put(new JSONObject("{\"title\": \"Включи все\",\"hide\": true}"));
+                    responseBody.put("buttons",buttons);
+                    break;
+                default:
+                    responseBody.put("text","Я Вас не понимаю");
+                    break;
+            }
+        }
         responseBody.put("end_session",false);
         response.put("response",responseBody);
-
-        System.out.println(request.get("version"));
         return response;
     }
 }
